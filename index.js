446668f76ee9101cdd196e9e4cbe36a6f476c2b3
Jay Friedmann <jay@swissmediainc.net>
2019-03-08 01:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function sayHiToGrandma(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+  if (string === "I love you, Grandma.") {
+    return "I love you, too!"
   }
 }
